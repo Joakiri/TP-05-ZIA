1626d43f89408eef87355ab974a4b9dec3a8de84
JOAQUÍN IRIBAS <49627045@ort>
2025-06-05 15:56:50 -0300

    Views, controllers y models

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -16,4 +16,36 @@ public class HomeController : Controller
     {
         return View();
     }
+    
+    public IActionResult play (string name){
+        Partida partida = new Partida(name);
+        partida.moveFoward();
+        HttpContext.Session.SetString("partida", Objeto.ObjectToString(partida));
+        return View("Habitacion1");
+    }
+    public IActionResult fromXToY(string answer){
+
+        Partida partida = Objeto.StringToObject<Partida>(HttpContext.Session.GetString("partida"));
+        int i = partida.moveFowardForm(answer);
+        return View(i);
+    }
+     public IActionResult fromXToYButton(string answer){
+        Partida partida = Objeto.StringToObject<Partida>(HttpContext.Session.GetString("partida"));
+        int i = partida.moveFowardButton();
+        return View(i);
+    }
+    public IActionResult IngresarNombre()
+    {
+        Partida partida = Objeto.StringToObject<Partida>(HttpContext.Session.GetString("partida"));
+        ViewBag.name = partida.name;
+        return View("IngresarNombre");
+    }
+    public IActionResult MostrarTutorial()
+    {
+        return View("Tutorial");
+    }
+    public IActionResult MostrarIntegrantes()
+    {
+        return View("Integrantes");
+    }
 }
--- Models/Partida.cs
+++ Models/Partida.cs
@@ -1,4 +1,4 @@
-namespace TP_04_ZIA;
+namespace TP_05_ZIA;
 using Newtonsoft.Json;
 using System.Timers;
 public class Partida
@@ -11,9 +11,30 @@ public class Partida
     [JsonProperty]
     public Timer timer {get; private set;}
 
+    [JsonProperty]
+    public Dictionary <int, string> hints {get; private set;}
+
+    public string[] correctAnswers  {get; private set;}
     public Partida(String name){
         this.currentRoom = 0;
         this.name = name;
-        this.timer = new Timer(100);
+        this.timer = new Timer(500);
+        this.hints = new Dictionary<int, string>();
+        hints.Add(1, "Encontrar la llave para poder salir de la celda");hints.Add(2, "Encontra el codido par abrir la puerta");hints.Add(3, "Me usan para entrar, me ocultan para escapar.Sin mí, la cerradura no se puede desbloquear. No soy palabra ni truco, pero soy la clave del lugar. ¿Qué soy?");hints.Add(4, "Cada puerta tiene un cartel, pero solo uno de ellos dice la verdad. Los otros dos mienten.Puerta 1: 'La puerta 2 conduce a la libertad.'Puerta 2: 'No saldrás por esta puerta.'Puerta 3: 'La puerta 1 miente.'");
+        this.correctAnswers = new string[4];
+        correctAnswers[1] = ""; correctAnswers[2] = "";  correctAnswers[3] = "";  correctAnswers[4] = "";     
+    }
+    public void moveFoward(){
+        
+    }
+    public int moveFowardForm(string answer){
+        if(answer == correctAnswers[currentRoom]){
+            currentRoom++;
+        }
+        return currentRoom;
+    }
+    public int moveFowardButton(){
+        currentRoom++;
+        return currentRoom;
     }
 }
