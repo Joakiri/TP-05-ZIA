39a6cdc7ed833030dd78542a6374b6cc0c6c0fc4
JOAQUÍN IRIBAS <49627045@ort>
2025-06-12 15:59:41 -0300

    Habitacion 1

--- Models/Partida.cs
+++ Models/Partida.cs
@@ -22,7 +22,7 @@ public class Partida
         this.hints = new Dictionary<int, string>();
         hints.Add(1, "Encontrar la llave para poder salir de la celda");hints.Add(2, "Encontra el codido par abrir la puerta");hints.Add(3, "Me usan para entrar, me ocultan para escapar.Sin mí, la cerradura no se puede desbloquear. No soy palabra ni truco, pero soy la clave del lugar. ¿Qué soy?");hints.Add(4, "Cada puerta tiene un cartel, pero solo uno de ellos dice la verdad. Los otros dos mienten.Puerta 1: 'La puerta 2 conduce a la libertad.'Puerta 2: 'No saldrás por esta puerta.'Puerta 3: 'La puerta 1 miente.'");
         this.correctAnswers = new string[4];
-        correctAnswers[1] = ""; correctAnswers[2] = "";  correctAnswers[3] = "";  correctAnswers[4] = "";     
+        correctAnswers[0] = ""; correctAnswers[1] = "";  correctAnswers[2] = "";  correctAnswers[3] = "";     
     }
     public int moveFowardForm(string answer){
         if(answer == correctAnswers[currentRoom]){
